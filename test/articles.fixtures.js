5ed6a29325ba91bf225514551f903efd011ae70c
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-02-04 13:42:43 +0000

    removed unused and using INTEGER PRIMARY KEY

--- test/articles.fixtures.js
+++ test/articles.fixtures.js
@@ -37,12 +37,12 @@ function makeMaliciousArticle() {
     style: 'How-to',
     date_published: new Date().toISOString(),
     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
-    content:`Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`
+    content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`
   }
   const expectedArticle = {
     ...maliciousArticle,
     title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
-    content:`Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+    content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
   }
   return {
     maliciousArticle,
