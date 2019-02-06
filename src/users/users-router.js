a3b1e033803f9064c9194e24b6d002310fbefe52
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-02-05 22:07:34 -0400

    Add user service and router

---
+++ src/users/users-router.js
@@ -0,0 +1,106 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const UsersService = require('./users-service')
+
+const usersRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeUser = user => ({
+  id: user.id,
+  fullname: xss(user.fullname),
+  email: xss(user.email),
+  nickname: xss(user.nickname),
+})
+
+usersRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    UsersService.getAllUsers(knexInstance)
+      .then(users => {
+        res.json(users.map(serializeUser))
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { fullname, email, nickname, password } = req.body
+    const newUser = { fullname, email }
+
+    for (const [key, value] of Object.entries(newUser))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+    
+    newUser.nickname = nickname;
+    newUser.password = password;    
+
+    UsersService.insertUser(
+      req.app.get('db'),
+      newUser
+    )
+      .then(user => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${user.id}`))
+          .json(serializeUser(user))
+      })
+      .catch(next)
+  })
+
+usersRouter
+  .route('/:user_id')
+  .all((req, res, next) => {
+    UsersService.getById(
+      req.app.get('db'),
+      req.params.user_id
+    )
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({
+            error: { message: `User doesn't exist` }
+          })
+        }
+        res.user = user
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeUser(res.user))
+  })
+  .delete((req, res, next) => {
+    UsersService.deleteUser(
+      req.app.get('db'),
+      req.params.user_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { fullname, email, password, nickname } = req.body
+    const userToUpdate = { fullname, email, password, nickname }
+
+    const numberOfValues = Object.values(userToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'fullname', 'email', 'password' or 'nickname'`
+        }
+      })
+
+    UsersService.updateUser(
+      req.app.get('db'),
+      req.params.user_id,
+      userToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = usersRouter
