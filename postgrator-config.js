8094160a665fb3d0d7c4b91bff7f1bc813f59248
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-09-16 18:50:34 +0100

    adds production deployment config and settings

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,6 +4,7 @@ module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DB_URL
-    : process.env.DB_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
 }
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -16,7 +16,7 @@ describe('Articles Endpoints', function() {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     })
     app.set('db', db)
   })
